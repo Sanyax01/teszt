ebe122c204f6bd9dff6fbfeb9cecbed41327cb16
Petrik Diak <diak@petrik.hu>
2026-01-30 08:35:55 +0100

    Adatok osztaly kesz

---
+++ teszt/Adatok.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+	internal class Adatok
+	{
+
+
+		List<Adat> lista;
+		public Adatok() 
+		{
+			lista = new List<Adat>();
+		
+		}
+
+	}
+}
